74c88133dc10e15071739d69dec5effa94cae4c7
Sezim <amangulovasezim@gmail.com>
2024-04-14 14:44:03 +0100

    solved with counter

--- index.js
+++ index.js
@@ -1,54 +1,39 @@
-let func = () => {
-    let numbers3 = [];  
-    let nums;
 
-    while(true)  {
-        nums = +prompt ('enter value')
-        if (isNaN(nums)){ 
-        console.log('enter right digit')
-        continue;
-    } if (nums === 99){
-          break; 
-    }
-    numbers3.push(nums)
-    }
- const count = numbers3.reduce((prev)=> {
-    return prev + 1
-},0)
-  console.log(count)  
-    }
-func()
+//Task1
 
+let count = 0;
 
+let inputNum = +prompt ('Введите число');
 
-let functin = () => {
-    let num = [];
-    let addNum;
-     
-    while (true){
-        addNum = +prompt('Enter your digit')
-        if (isNaN(addNum)){
-            console.log('enter right digit')
-        continue;
-        }
-        if (addNum === 999) {
-            break;
-        }
-        num.push(addNum)
-    }
-    let count = num.reduce((prev) => {
-        return prev + 1
-    },0)
-    let sum = num.reduce((prev, item) => {
-        return prev+item
-    },0)
-    console.log(`Количество чисел ${count}, сумма ${sum} `)
+while(inputNum !== 99){
+    inputNum = +prompt ('Введите число');
+    count++
 }
+console.log(count);
 
-functin()
 
+
+//Task2
+
+let counter = 0;
+let sum = 0;
+
+let inputs = +prompt('Введите число 999');
+
+while (inputs !== 999){
+    counter++
+    sum = sum + inputs
+    inputs = +prompt('Введите число 999');
+  
+}
+console.log(`было введено ${counter}, а их сумма ${sum}`);
+
+
+
+//Task3
 
 const data = [['a', 1], ['b', 2]];
+
 let funciya = () => {
 let obj = {}
     for (let i = 0; i<data.length; i++){
@@ -58,6 +43,9 @@ console.log(obj)
 }
 funciya();
 
+
+
+//Task4
 
 const datas = [['a', 1], ['b', 3]];
 const obj = datas.reduce((acc, [key, value]) => 
